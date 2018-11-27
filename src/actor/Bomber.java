cd0deed6cf6b9316ba9b95b3b74a7446dea24e40
buihuynhduc <43197106+buihuynhduc@users.noreply.github.com>
2018-11-27 19:59:10 +0700

    Update Bomber.java

--- src/actor/Bomber.java
+++ src/actor/Bomber.java
@@ -1,32 +1,35 @@
-package actor;
+package actorgame;
 
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Rectangle;
 import java.util.ArrayList;
-
 import javax.swing.ImageIcon;
-
-import graphic.Graphic;
+import com.bhuy.bomb.gui.GUI;
 
 
 public class Bomber extends Actor{
 	public static int ALLOW_RUN=0;
 	public static int DISALLOW_RUN=1;
-	protected int sizeBomb,quantityBomb,status,score,heart;
+	protected int sizeBomb;
+        protected int quantityBomb;
+        protected int score;
+        
+               protected int status;
+               protected  int heart;
 	
 	
 	
-	public Bomber(int x, int y, int type, int orient, int speed, int sizebomb, int quantityBomb) {
-		this.x = x;
-		this.y = y;
-		this.type = type;
+	public Bomber(int a,int b,int c, int d, int s, int sz, int qu) {
+		this.x = a;
+		this.y = b;
+		this.type = c;
 		this.runBomb=DISALLOW_RUN;
-		this.orient = orient;
-		this.speed = speed;
-		this.sizeBomb = sizebomb;
-		this.quantityBomb = quantityBomb;
-		this.heart = 3;
+		this.orient = d;
+		this.speed = s;
+		this.sizeBomb = sz;
+		this.quantityBomb = qu;
+		this.heart = 2;
 		this.score=0;
 		this.status = Actor.ALIVE;
 		this.img = new ImageIcon(getClass().getResource("/Images/bomber_right.png")).getImage();
@@ -34,106 +37,127 @@ public class Bomber extends Actor{
 		height = img.getHeight(null)-20;
 	}
 
-	public void setNew(int x,int y) {
-		this.x = x;
-		this.y = y;
+	public void setNew(int a,int b)
+        {
+		this.x = a;
+		this.y = b;
 		this.status = ALIVE;
 		this.img = new ImageIcon(getClass().getResource("/Images/bomber_right.png")).getImage();
 	}
-	public int getScore() {
-		return score;
-	}
-
-	public void setScore(int score) {
+        public void setScore(int score) 
+        {
 		this.score = score;
 	}
 
-	public void setImg(Image img) {
-		this.img = img;
+	public int getScore()
+        {
+		return score;
+	}
+
+	
+	public void setImg(Image im) {
+		this.img = im;
 	}
 	
-	public int getStatus() {
+	public int getStatus()
+        {
 		return status;
 	}
 
-	public void setStatus(int status) {
-		this.status = status;
+	public void setStatus(int st)
+        {
+		this.status = st;
 	}
 
 	public int getQuantityBomb() {
 		return quantityBomb;
 	}
 
-	public void setQuantityBomb(int quantityBomb) {
-		if(quantityBomb>5){
+	public void setQuantityBomb(int quantityBomb) 
+        {
+		if(quantityBomb>5)
+                {
 			return;
 		}
 		this.quantityBomb = quantityBomb;
 	}
 
 
-	public void setSizeBomb(int sizeBomb) {
-		if(sizeBomb>3){
+	public void setSizeBomb(int sizeBomb)
+        {
+		if(sizeBomb>3)
+                {
 			return;
 		}
 		this.sizeBomb = sizeBomb;
 	}
 
-	public int getSizeBomb() {
+	public int getSizeBomb()
+        {
 		return sizeBomb;
 	}
 
-	public int getType() {
+	public int getType()
+        {
 		return type;
 	}
 	
-	public int getHeart() {
+	public int getHeart()
+        {
 		return heart;
 	}
 
-	public void setHeart(int heart) {
-		this.heart = heart;
+	public void setHeart(int tym) {
+		this.heart = tym;
 	}
 
 	@Override
-	public boolean move(int count, ArrayList<Bomb> arrBomb, ArrayList<Box> arrBox) {
-		if(status==DEAD){
+	public boolean move(int c, ArrayList<Bomb> ab, ArrayList<Box> ax)
+        {
+		if(status==DEAD)
+                {
 			return false;
 		}
-		return super.move(count, arrBomb, arrBox);
+		return super.move(c, ab, ax);
+	}
+        public boolean isImpactBomberVsActor(Actor ac)
+        {
+		if(status==DEAD)
+                {
+			return false;
+		}
+		Rectangle r1 = new Rectangle(x,y,width,height);
+		Rectangle r2 = new Rectangle(ac.getX(),ac.getY(),ac.getWidth(),ac.getHeight());
+		return r1.intersects(r2);
 	}
 	
 	@Override
-	public void changeOrient(int orient) {
+	public void changeOrient(int o)
+        {
 		if(this.status==DEAD){
 			return;
 		}
-		super.changeOrient(orient);
-		switch (orient) {
-		case LEFT:
-			img = new ImageIcon(getClass().getResource("/Images/bomber_left.png")).getImage();
-			break;
+		super.changeOrient(o);
+		switch (o) {
+		
 		case RIGHT:
 			img = new ImageIcon(getClass().getResource("/Images/bomber_right.png")).getImage();
 			break;
-		case UP:
-			img = new ImageIcon(getClass().getResource("/Images/bomber_up.png")).getImage();
-			break;
+		
 		case DOWN:
 			img = new ImageIcon(getClass().getResource("/Images/bomber_down.png")).getImage();
+			break;
+                case LEFT:
+			img = new ImageIcon(getClass().getResource("/Images/bomber_left.png")).getImage();
+			break;
+                case UP:
+			img = new ImageIcon(getClass().getResource("/Images/bomber_up.png")).getImage();
 			break;
 		default:
 			break;
 		}
 	}
 
-	public boolean isImpactBomberVsActor(Actor actor){
-		if(status==DEAD){
-			return false;
-		}
-		Rectangle rec1 = new Rectangle(x, y, width, height);
-		Rectangle rec2 = new Rectangle(actor.getX(), actor.getY(), actor.getWidth(), actor.getHeight());
-		return rec1.intersects(rec2);
-	}
+	
 	
 }
